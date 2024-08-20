80cd1604d2583b3bae25fb71baf1c23878710242
Dullya Petronilio <petroniliodullya@gmail.com>
2024-08-19 21:50:48 -0300

    primeiro desafio

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class ContaTerminal {
+public class ContaTerminal {
+
+    int numero = 0;
+    String agencia;
+    String nomeClinete;
+    float  saldo = 0;
+
+
+    public  void numero(int nummeroConta ){
+
+        numero = nummeroConta;
+
+
+    }
+
+    public void informarAgencia(String numAgencia) {
+
+        agencia = numAgencia;
+    }
+
+    public void incluirNome(String novoNome) {
+
+        nomeClinete = novoNome;
+    }
+
+    public void depositar(float novoDesposito) {
+
+        saldo = novoDesposito;
+    }
+
+
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Usuario {
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.zip.DeflaterOutputStream;
+
+public class Usuario {
+
+    public static void main(String[] args) {
+
+
+        ContaTerminal contaTerminal = new ContaTerminal();
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Por favor, digite o número de conta: ");
+        int numeroDeConta = scanner.nextInt();
+        contaTerminal.numero(numeroDeConta);
+
+
+        System.out.println("Por favor, digite o número da agência: " );
+        String numeroAg = scanner.next();
+        contaTerminal.informarAgencia(numeroAg);
+
+
+        System.out.println("Por favor, digite o seu nome: ");
+        String nome = scanner.next();
+        contaTerminal.incluirNome(nome);
+
+
+        System.out.println("Por favor, digite o valor para despositar: ");
+        float saldoConta = scanner.nextFloat();
+        contaTerminal.depositar(saldoConta);
+
+
+
+
+        System.out.println("Olá " +contaTerminal.nomeClinete + " , obrigada por criar uma conta em nosso banco, sua agência é " + contaTerminal.agencia + " , conta " + contaTerminal.numero + " e eu saldo " + contaTerminal.saldo + " já está disponivel para saque.");
+
+        scanner.close();
+    }
 }
